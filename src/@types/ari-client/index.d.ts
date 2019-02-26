7853f354046742cdfeff2bc79a4142d856dc51a5
Dementyev AlexeyHome <adementyev@zaoblako.ru>
2019-02-27 02:01:48 +0300

    [FEAT] implement skeleton of some methods, write enums

--- src/@types/ari-client/index.d.ts
+++ src/@types/ari-client/index.d.ts
@@ -5,36 +5,104 @@
 
 declare module 'ari-client' {
 
-    export type AriClientEvent = 'StasisStart';
+    export enum AriClientEvent {
+        APILoadError,
+        ApplicationReplaced,
+        StasisStart,
+        ChannelDtmfReceived,
+        BridgeAttendedTransfer,
+        WebSocketReconnecting,
+        WebSocketConnected,
+        WebSocketMaxRetries
+    }
+
+    export enum AriBridgeEvent {
+        BridgeBlindTransfer,
+        BridgeCreated,
+        BridgeDestroyed,
+        BridgeMerged,
+        BridgeVideoSourceChanged,
+        ChannelCallerId,
+        ChannelHangupRequest,
+        ChannelStateChange,
+        ChannelVarset
+    }
+
+    export enum AriChannelEvent {
+        StasisStart,
+        ChannelDtmfReceived,
+        ChannelConnectedLine,
+        ChannelCreated,
+        ChannelDestroyed,
+        ChannelDialplan,
+        ChannelEnteredBridge,
+        ChannelHold,
+        ChannelLeftBridge,
+        ChannelTalkingFinished,
+        ChannelTalkingStarted,
+        ChannelUnhold,
+        ChannelUserevent,
+        ContactStatusChange,
+        EndpointStateChange,
+        StasisEnd
+    }
+
+    export enum AriEndpointEvent {
+        DeviceStateChanged,
+        MissingParams,
+        Peer,
+        TextMessageReceived,
+        PeerStatusChange,
+        PlaybackContinuing
+    }
+
+    export enum AriPlaybackEvent {
+        PlaybackStarted,
+        RecordingFailed,
+        RecordingFinished,
+        RecordingStarted
+    }
+
+    export enum AriDeviceStateEvent {
+        Dial
+    }
+
     export type AriEvent = AriStasisStartEvent;
+
+    type AriEventChannelCallback = (event: AriEvent, channelInstance: AriChannel) => void;
 
     export interface AriInstance {
 
+        applications: AriApplication;
         asterisk: AriAsterisk;
         bridges: AriBridge;
         channels: AriChannel;
-        endpoints: any;
-        applications: AriApplication;
+        deviceStates: AriDeviceState;
+        endpoints: AriEndpoint;
+        mailboxes: AriMailbox;
+        playbacks: AriPlayback;
+        recordings: AriRecording;
+        sounds: any;
 
-        on(clientEvent: AriClientEvent, cb: (event: AriEvent, channelInstance: AriChannel) => void): void;
+        on(clientEvent: AriClientEvent | keyof typeof AriClientEvent, cb: AriEventChannelCallback): void;
 
-        once(clientEvent: AriClientEvent, cb: (event: AriEvent, channelInstance: AriChannel) => void): void;
-
-        answer(cb: (err?: any) => void): void;
-
-        answer(): Promise<null>;
+        once(clientEvent: AriClientEvent | keyof typeof AriClientEvent, cb: AriEventChannelCallback): void;
 
     }
 
+    type AriErrCallback = (err: any) => void;
+
+    type AriNullPromise = Promise<null>;
+
     export interface AriAsterisk {
 
-        addLog(options: { configuration: string, logChannelName: string }, cb: (err: any) => void): void;
+        addLog(options: { configuration: string, logChannelName: string }, cb: AriErrCallback): void;
 
-        addLog(options: { configuration: string, logChannelName: string }): Promise<null>;
+        addLog(options: { configuration: string, logChannelName: string }): AriNullPromise;
 
-        deleteLog(options: { logChannelName: string }, cb: (err: any) => void): void;
+        deleteLog(options: { logChannelName: string }, cb: AriErrCallback): void;
 
-        deleteLog(options: { logChannelName: string }): Promise<null>;
+        deleteLog(options: { logChannelName: string }): AriNullPromise;
 
         deleteObject(): void;
 
@@ -63,40 +131,169 @@ declare module 'ari-client' {
         updateObject(): void;
     }
 
+    type AriDataCallback = (err: any, data: any) => void;
+
+    type AriDataPromise = Promise<any>;
+
     export interface AriApplication {
-        get(options: {
-                applicationName: string
-            },
-            cb: (err: any, application: any) => void): void;
+        get(options: { applicationName: string }, cb: AriDataCallback): void;
 
-        get(options: { applicationName: string }): Promise<any>;
+        get(options: { applicationName: string }): AriDataPromise;
 
-        list(cb: (err: any, applications: any) => void): void;
+        list(cb: AriDataCallback): void;
 
-        list(): Promise<any>;
+        list(): AriDataPromise;
 
-        subscribe(options: {
-            applicationName: string,
-            eventSource: string
-        }, cb: (err: any, application: any) => void): void;
+        subscribe(options: { applicationName: string, eventSource: string }, cb: AriDataCallback): void;
 
-        subscribe(options: {
-            applicationName: string,
-            eventSource: string
-        }): Promise<any>;
+        subscribe(options: { applicationName: string, eventSource: string }): AriDataPromise;
 
-        unsubscribe(options: {
-            applicationName: string,
-            eventSource: string
-        }, cb: (err: any, application: any) => void): void;
+        unsubscribe(options: { applicationName: string, eventSource: string }, cb: AriDataCallback): void;
 
-        unsubscribe(options: {
-            applicationName: string,
-            eventSource: string
-        }): Promise<any>;
+        unsubscribe(options: { applicationName: string, eventSource: string }): AriDataPromise;
 
 
     }
+
+    export interface AriEndpoint {
+        get(): void;
+
+        list(): void;
+
+        listByTech(): void;
+
+        sendMessage(): void;
+
+        sendMessageToEndpoint(): void;
+    }
+
+    export interface AriSound {
+        get(): void;
+
+        list(): void;
+
+    }
+
+    export interface AriRecording {
+        cancel(): void;
+
+        copyStored(): void;
+
+        deleteStored(): void;
+
+        getLive(): void;
+
+        getStored(): void;
+
+        getStoredFile(): void;
+
+        listStored(): void;
+
+        mute(): void;
+
+        pause(): void;
+
+        stop(): void;
+
+        unmute(): void;
+
+        unpause(): void;
+    }
+
+    export interface AriPlayback {
+        control(): void;
+
+        get(): void;
+
+        stop(): void;
+    }
+
+    export interface AriMailbox {
+        delete(): void;
+
+        get(): void;
+
+        list(): void;
+
+        update(): void;
+    }
+
+    export interface AriDeviceState {
+        delete(): void;
+
+        get(): void;
+
+        list(): void;
+
+        update(): void;
+    }
+
+    type AriChannelContinueInDialplanOptions = {
+        channelId: string,
+        context: string,
+        extension: string,
+        label: string,
+        priority: number
+    };
+
+    type AriChannelCreateOptions = {
+        app: string,
+        appArgs: string,
+        channelId: string,
+        endpoint: string,
+        formats: string,
+        originator: string,
+        otherChannelId: string
+    };
+
+    type AriChannelDialOptions = {
+        caller: string,
+        channelId: string,
+        timeout: number
+    };
+
+    type AriStdCallback = (err: any, channel: AriChannel) => void;
+
+    type AriChannelOriginateOptions = {
+        app: string,
+        appArgs: string,
+        callerId: string,
+        channelId: string,
+        context: string,
+        endpoint: string,
+        extension: string,
+        formats: string,
+        label: string,
+        originator: string,
+        otherChannelId: string,
+        priority: number,
+        timeout: number,
+        variables: object
+    };
+
+    type AriStdPromise = Promise<AriChannel>;
+
+    type AriChannelPlayOptions = {
+        channelId: string,
+        lang: string,
+        media: string,
+        offsetms: number,
+        playbackId: string,
+        skipms: number
+    };
+
+    type AriPlaybackPromise = Promise<AriPlayback>;
+
+    type AriChannelRecordOptions = {
+        channelId: string,
+        format: string,
+        name: string,
+        beep?: boolean,
+        ifExists?: string,
+        maxDurationSeconds?: number,
+        maxSilenceSeconds?: number,
+        terminateOn?: number
+    };
 
     export interface AriChannel {
         id: string;
@@ -107,15 +304,91 @@ declare module 'ari-client' {
         creationtime: Date;
         dialplan: AriDialplanCEP;
 
-        answer(): void;
+        on(channelEvent: AriChannelEvent | keyof typeof AriChannelEvent, cb: AriEventChannelCallback): void;
 
-        continueInDialplan(): void;
+        answer(options: { channelId: string }, cb: AriErrCallback): void;
 
-        create(): void;
+        answer(options: { channelId: string }): AriNullPromise;
 
-        dial(): void;
+        continueInDialplan(options: AriChannelContinueInDialplanOptions, cb: AriErrCallback): void;
 
-        get(): void;
+        continueInDialplan(options: AriChannelContinueInDialplanOptions): AriNullPromise;
+
+        create(options: AriChannelCreateOptions, cb: AriStdCallback): void;
+
+        create(options: AriChannelCreateOptions): AriStdPromise;
+
+        dial(options: AriChannelDialOptions, cb: AriErrCallback): void;
+
+        dial(options: AriChannelDialOptions): AriNullPromise;
+
+        get(options: { channelId: string }, cb: AriStdCallback): void;
+
+        get(options: { channelId: string }): AriStdPromise;
+
+        getChannelVar(options: { channelId: string, variable: string }, cb: AriDataCallback): void;
+
+        getChannelVar(options: { channelId: string, variable: string }): AriDataPromise;
+
+        hangup(options: { channelId: string, reason: string }, cb: AriErrCallback): void;
+
+        hangup(options: { channelId: string, reason: string }): AriNullPromise;
+
+        hold(options: { channelId: string }, cb: AriErrCallback): void;
+
+        hold(options: { channelId: string }): AriNullPromise;
+
+        list(cb: AriDataCallback): void;
+
+        list(): AriDataPromise;
+
+        mute(options: { channelId: string, direction: string }, cb: AriErrCallback): void;
+
+        mute(options: { channelId: string, direction: string }): AriNullPromise;
+
+        originate(options: AriChannelOriginateOptions, cb: AriStdCallback): void;
+
+        originate(options: AriChannelOriginateOptions): AriStdPromise;
+
+        originateWithId(options: AriChannelOriginateOptions, cb: AriStdCallback): void;
+
+        originateWithId(options: AriChannelOriginateOptions): AriStdPromise;
+
+        play(options: AriChannelPlayOptions, cb: (err: any, playback: AriPlayback) => void): void;
+
+        play(options: AriChannelPlayOptions): AriPlaybackPromise;
+
+        playWithId(options: AriChannelPlayOptions, cb: (err: any, playback: AriPlayback) => void): void;
+
+        playWithId(options: AriChannelPlayOptions): AriPlaybackPromise;
+
+        record(options: AriChannelRecordOptions, cb: (err: any, recording: AriRecording) => void): void;
+
+        record(options: AriChannelRecordOptions): Promise<AriRecording>;
+
+        redirect(): void;
+
+        ring(): void;
+
+        ringStop(): void;
+
+        sendDTMF(): void;
+
+        setChannelVar(): void;
+
+        snoopChannel(): void;
+
+        snoopChannelWithId(): void;
+
+        startMoh(): void;
+
+        startSilence(): void;
+
+        stopMoh(): void;
+
+        stopSilence(): void;
+
+        unhold(): void;
     }
 
     export interface AriStasisStartEvent {
@@ -154,7 +427,7 @@ declare module 'ari-client' {
 
         list(cb: (err: any, bridges: any[]) => void): void;
 
-        list(): Promise<any>;
+        list(): AriDataPromise;
 
         play(): void;
 
